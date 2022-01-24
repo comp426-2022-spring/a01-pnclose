9b7077356118e0340a12529626ef8fdb98e50077
Peter Close <pclose99@gmail.com>
2022-01-24 15:13:00 -0500

    make notes for understanding

--- server.js
+++ server.js
@@ -1,58 +1,44 @@
-// Require http module
+// WHY: You need this to make a request over the hyper text transfer protocol (http)
 const http = require('http')
 
-// Require fs module
+// WHY: Helps us store, access, and manage data on our operating system. We use it for readFile
 const fs = require('fs')
 
-// Require minimist module (make sure you install this one via npm).
-// Use minimist to process one argument `--port=` on the command line after `node server.js`.
+
+// WHY: To process one argument `--port=` on the command line after `node server.js`
+// WHAT IS argv: Returns an argument object argv populated with the array arguments from args
 const argv = require('minimist')(process.argv.slice(2))
+console.log(argv);
+
 
 
 // Define allowed argument name 'port'.
-// args['port']
+// çargs['port']
 
-// Define a const `port` using the argument from the command line. 
-// Make this const default to port 3000 if there is no argument given for `--port`.
+// WHY: We set the port in argv to be 3000
 const port = argv['port'] || 3000
 
-// Use the fs module to create an arrow function using `fs.readFile`.
-// Use the documentation for the Node.js `fs` module. 
-// The function must read a file located at `./www/index.html` and do some stuff with it.
-// The stuff that should be inside this function is all below.
+
+// WHY: We read file index.html since that is what we want displayed on the server
+// if err then return exit(1)
 fs.readFile('./www/index.html', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return process.exit(1)
     }
 
+    // create the server, and set the 3 things
+    // statusCode, header, end
     const server = http.createServer((req, res) => {
         res.statusCode = 200
         res.setHeader('Content-Type', 'text/html')
         res.end(data)
     })
 
+    // listen for output
     server.listen(port, () => {
         console.log(`Server listening on port ${port}`)
     })
 })
-
-// If there is an error, put it on the console error, return, and exit with error code 1. 
-// Do not be nice about exiting.
-
-
-
-
-
-// Define a const `server` as an arrow function using http.createServer. 
-// Use the documentation for the node.js http module. 
-// The function should have three responses: 
-// 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./www/index.html.
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-
 
 // That's it! You're all done!
