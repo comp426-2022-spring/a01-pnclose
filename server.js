a5e396088086b3b3d71e7ed4f8ca364434f55085
Peter Close <pclose99@gmail.com>
2022-01-24 14:18:41 -0500

    add port and readFile

--- server.js
+++ server.js
@@ -6,19 +6,27 @@ const fs = require('fs')
 
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2))
 
 
 // Define allowed argument name 'port'.
-
+args['port']
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+const port = process.env.port || 3000
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+fs.readFile('./www/index.html', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return process.exit(1)
+      }
+
+})
 
 // If there is an error, put it on the console error, return, and exit with error code 1. 
 // Do not be nice about exiting.
